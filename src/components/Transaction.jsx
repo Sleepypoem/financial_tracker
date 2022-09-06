b57b3848104ef1251bd3cc48bde1b1a65c895e3c
Alexander <gallade2014@gmail.com>
2022-09-06 16:14:31 -0600

    Delete transaction button now working.

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 function Transaction(props) {
 
-    const { date, category, amount } = props;
+    const { id, date, category, amount, deleteTransaction } = props;
+
+    const handleClick = () => {
+        deleteTransaction(id);
+    }
+
     return (
         <tr className="">
             <td >{date}</td>
             <td>{category}</td>
             <td>{amount}</td>
-            <td><button className='btn btn-danger btn-sm'>X</button></td>
+            <td><button onClick={handleClick} className='btn btn-danger btn-sm'>X</button></td>
         </tr>
     );
 }
--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -3,11 +3,11 @@ import Transaction from './Transaction';
 
 function TransactionList(props) {
 
-    const { transactions } = props;
+    const { transactions, deleteTransaction } = props;
 
     return (<div className='container w-25'>
         <div className="table-responsive-md">
-            <table className="table table-primary">
+            <table className="table table-primary table-hover">
                 <thead>
                     <tr>
                         <th scope="col">Date</th>
@@ -17,9 +17,9 @@ function TransactionList(props) {
                     </tr>
                 </thead>
                 <tbody>
-                    {transactions.length === 0 ? <tr><td colspan="3" className='text-center'>No transactions</td></tr> :
+                    {transactions.length === 0 ? <tr><td colSpan="3" className='text-center'>No transactions</td></tr> :
                         transactions.map((transaction) => {
-                            return <Transaction date={transaction.date} category={transaction.category} amount={transaction.amount} />
+                            return <Transaction deleteTransaction={deleteTransaction} key={transaction.id} id={transaction.id} date={transaction.date} category={transaction.category} amount={transaction.amount} />
                         })}
                 </tbody>
             </table>
